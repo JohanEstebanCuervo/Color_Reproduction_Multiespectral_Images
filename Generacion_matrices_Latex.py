ad28846e87b82f6badcdcb437b2542f733754737
DESKTOP-KQAFMU2\Johan Cuervo <jecuervoch@unal.edu.co>
2021-11-13 00:27:10 -0500

    Version 2 de generacion de archivo .TEX para matrices

--- Generacion_matrices_Latex.py
+++ Generacion_matrices_Latex.py
@@ -8,25 +8,33 @@ Created on Fri Nov 12 23:10:40 2021
 import sympy as sym
 import numpy as np
 import funciones_reproduccion_color as fun
+import os
 
-CCm = fun.Read_Variable('Resultados/Variables/CCM_Compound.pickle')
+carpeta = 'Resultados/Variables'
+lista = os.listdir(carpeta)
 
 
-file = open('Resultados/Formulas_Latex/prueba.tex','w')
-matrix= sym.latex(sym.Matrix(CCm.round(3)), full_prec=False)
-texto='\n'
-n=0
-for i,carac in enumerate(matrix):
-    if carac=='\\' and matrix[i+1] == '\\':
-      texto+=matrix[n:i+1]+'\\'+' \n'
-      n=i+2
-
-texto+=matrix[n:]
-
-file.write('\\begin{ecuation}\n')
-file.write('\\begin{bmatrix}\n  R \\\ G \\\ B \n\end{bmatrix}=')
-file.write(texto)
-file.write('\n\\begin{bmatrix}\n  R \\\ G \\\ B \\\ 1 \n\end{bmatrix}\n')
-file.write('\end{ecuation}\n')
-
-file.close()
+for nombre in lista:
+    if nombre[:4]=='CCM_':
+        CCm = fun.Read_Variable(carpeta +'/'+ nombre)
+        
+        nombre_archivo = nombre[:nombre.find('.')]
+        file = open('Resultados/Formulas_Latex/'+nombre_archivo+'.tex','w')
+        matrix= sym.latex(sym.Matrix(CCm.round(3)), full_prec=False)
+        texto='\n'
+        n=0
+        
+        for i,carac in enumerate(matrix):
+            if carac=='\\' and matrix[i+1] == '\\':
+              texto+=matrix[n:i+1]+'\\'+' \n'
+              n=i+2
+        
+        texto+=matrix[n:]
+        
+        file.write('\\begin{ecuation}\n')
+        file.write('\\begin{bmatrix}\n  R \\\ G \\\ B \n\end{bmatrix}=')
+        file.write(texto)
+        file.write('\n\\begin{bmatrix}\n  R \\\ G \\\ B \\\ 1 \n\end{bmatrix}\n')
+        file.write('\end{ecuation}\n')
+        
+        file.close()

7529bf20abb82bb37e2d42dfd4bc93b9329514ae
JohanEstebanCuervo <88335518+JohanEstebanCuervo@users.noreply.github.com>
2022-03-27 03:40:13 -0500

    Mejora al lector de puertos serial

--- Adquisicion de imagenes/puerto_automatico.py
+++ Adquisicion de imagenes/puerto_automatico.py
@@ -12,18 +12,52 @@ import os
 import subprocess
 import platform
 
-sistema= platform.system()
 
-
-x = str(subprocess.check_output('python -m serial.tools.list_ports',shell=True),'UTF-8')
-
-lista = []
-
-inicio=0
-i=x.find(' ',inicio,len(x))
-while i>0:
-    lista.append(x[inicio:i])
-    inicio=x.find('\n',inicio,len(x))+1
+def Serial_Port_Select(port=0):
+    
+    if port!=1:
+        return port
+    
+    x = str(subprocess.check_output('python -m serial.tools.list_ports',shell=True),'UTF-8')
+    lista = []
+    inicio=0
     i=x.find(' ',inicio,len(x))
+    while i>0:
+        lista.append(x[inicio:i])
+        inicio=x.find('\n',inicio,len(x))+1
+        i=x.find(' ',inicio,len(x))
+    
+    if len(lista)==0:
+        print("No hay puertos seriales conectados")
+        return 1
+    
+    else:
+        print("Puertos: ")
+        if len(lista)==1:
+            print(lista[0])
+            
+            return lista[0]
+        
+        for i in range(len(lista)):    
+            print(str(i+1)+'. '+lista[i])
+          
+        correct=0
+        while correct==0:
+            try:
+                sel=int(input("Seleccione numero de puerto: "))
+                if sel>0 and sel<=len(lista):
+                    correct=1
+                
+                else:
+                    print("Valor erroneo ingrese el numero correspondiene al puerto")
+                
+            except:
+                print("Valor erroneo ingrese el numero correspondiene al puerto")
+                
+        return lista[sel-1]
+        
+nombre = Serial_Port_Select()
 
-print(lista)
+print(nombre)
+
+

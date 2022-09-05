7d13e66ddce5202306b7be7120cbe241e7aeab90
JohanEstebanCuervo <88335518+JohanEstebanCuervo@users.noreply.github.com>
2022-09-05 00:23:36 -0500

    DEL: Se purgo elementos del proyecto en deshuso

--- pruebas.py
+++
@@ -1,8 +0,0 @@
-import funciones_reproduccion_color as fun
-import numpy as np
-
-folder = "Resultados/Variables"
-nombre = 'errores_de_ReyCorrec_Nim_4.pickle'
-
-errores = fun.Read_Variable(folder + "/" + nombre).round(3)
-print(np.shape(errores))
